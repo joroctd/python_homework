a1f14ff6c299c50f10e73770e48784dc3814f4bf
johnoro <officialjohnorourke@gmail.com>
2025-07-28 20:43:11 -0400

    completes type-decorator.py

---
+++ assignment3/type-decorator.py
@@ -0,0 +1,23 @@
+def type_convertor(type_of_output):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            x = func(*args, **kwargs)
+            return type_of_output(x)
+        return wrapper
+    return decorator
+
+@type_convertor(str)
+def return_int():
+    return 5
+
+@type_convertor(int)
+def return_string():
+    return 'not a number'
+
+y = return_int()
+print(type(y).__name__) # This should print "str"
+try:
+   y = return_string()
+   print("shouldn't get here!")
+except ValueError:
+   print("can't convert that string to an integer!") # This is what should happen
