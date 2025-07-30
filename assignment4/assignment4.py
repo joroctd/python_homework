05b023ecab7b28a69f011022c2d8fd2b901c4561
johnoro <officialjohnorourke@gmail.com>
2025-07-29 21:29:16 -0400

    sets up assignment4 dir

---
+++ assignment4/assignment4.py
@@ -0,0 +1,59 @@
+# 1.1 Create a DataFrame from a dictionary
+
+
+# 1.2 Add a new column
+
+
+# 1.3 Modify an existing column
+
+
+# 1.4 Save the DataFrame as a CSV file
+
+
+# ---
+
+# 2.1 Read data from a CSV file
+
+
+# 2.2 Read data from a JSON file
+
+
+# 2.3 Combine DataFrames
+
+
+# ---
+
+# 3.1 Use the head() method
+
+
+# 3.2 Use the tail() method
+
+
+# 3.3 Get the shape of a DataFrame
+
+
+# 3.4 Use the info() method
+
+
+# ---
+
+# 4.1 Create a DataFrame from dirty_data.csv file and assign it to the variable dirty_data
+
+
+# 4.2 Remove any duplicate rows from the DataFrame
+
+
+# 4.3 Convert Age to numeric and handle missing values
+
+
+# 4.4 Convert Salary to numeric and replace known placeholders (unknown, n/a) with NaN
+
+
+# 4.5 Fill missing numeric values (use fillna).  Fill Age which the mean and Salary with the median
+
+
+# 4.6 Convert Hire Date to datetime
+
+
+# 4.7 Strip extra whitespace and standardize Name and Department as uppercase
+
